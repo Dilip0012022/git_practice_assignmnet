a01edb4f123b39e7f57cf86d3475971974c3b259
Dilip Kumar Gupta <dkguptaji001@gmail.com>
2023-01-07 11:24:01 +0530

    first code done

---
+++ prime_or_not.js
@@ -0,0 +1,11 @@
+let n=5;
+for(let i=2;i<n;i++){
+  if(n%i==0){
+     count++;
+  }
+}
+if(count==0){
+  console.log("Not a Prime Number");  //It's a mistake----"Make it Right"
+}else{
+  console.log("Prime Number");        //It's a mistake----"Make it Right"
+}
