bb8d5cc9dfb283bd6194cc0471de6803c7cdae49
vk10082000 <vamshikrishna99089@gmail.com>
2023-01-13 20:26:27 +0530

    check my code

---
+++ even.js
@@ -0,0 +1,16 @@
+let num=67;
+let count=0;
+for(let i=1;i<=num;i++)
+{
+if(num%i==0)
+{
+count++;
+}
+}
+if(count==2)
+{
+console.log(num,"is a Prime number");
+}
+else{
+console.log(num,"is not a Prime number");
+}
